9cab0fff2e295f0e3a8c2ef71974a991c5ef722b
zfarmertic <lilyigbunu@gmail.com>
2025-11-04 14:21:42 +0100

    square

---
+++ 8-square.js
@@ -0,0 +1,20 @@
+const args = process.argv;
+const parsed = parseInt(args[2]);
+
+let row = 0;
+if (isNaN(parsed)){
+    console.log("Missing size")
+}else {
+    let col = 0;
+    while ( col < parsed){
+        let line = ''
+        let row = 0;
+    while(row < parsed){
+            line += "x"
+            console.log(line)
+            row++;
+        }
+        // console.log(line)
+        col++;
+    }
+}
