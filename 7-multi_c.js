2fb78228643f9eba2ed0667adf06f09faa0e874f
zfarmertic <lilyigbunu@gmail.com>
2025-11-04 13:59:01 +0100

    the loop and repeation task

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,11 @@
 const args = process.argv
 const parsed = parseInt(args[2])
 let i = 0
-if(isNaN(parsed)){
+if (isNaN(parsed)){
     console.log("Missing number of occurrences")
-}else{
+} else {
     while ( i < parsed){
         console.log("C is fun")
-        i++
-        console.log(i)
+        i++;
     }
 }
